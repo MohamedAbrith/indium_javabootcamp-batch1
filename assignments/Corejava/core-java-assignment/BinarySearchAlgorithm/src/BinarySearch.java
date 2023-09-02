11d4f6bc814bbdb98fb5b0072037fd352db14830
MohamedAbrith <mohamedabrith24@gmail.com>
2023-09-02 22:08:46 +0530

    latest changes

---
+++ assignments/Corejava/core-java-assignment/BinarySearchAlgorithm/src/BinarySearch.java
@@ -0,0 +1,55 @@
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    static void bubbleSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+
+        }
+    }
+    static int binarySearch(int arr[], int key) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == key) {
+                return mid; // Element found, return its index
+            }
+            if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the size");
+        int size = scanner.nextInt();
+        int arr[]= new int[size];
+        System.out.println("enter the array elaments");
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("enter the key");
+        int key = scanner.nextInt();
+        bubbleSort(arr);
+        int result = binarySearch(arr, key);
+        if (result == -1) {
+            System.out.println("Element " + key + " not found in the array.");
+        } else {
+            System.out.println("Element " + key + " found at index " + result);
+        }
+    }
+}
