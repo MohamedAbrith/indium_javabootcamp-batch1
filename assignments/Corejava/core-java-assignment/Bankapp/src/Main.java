5f75e48cac33f6aec77a8aea6129e30eaec2be4f
MohamedAbrith <mohamedabrith24@gmail.com>
2023-09-11 11:58:47 +0530

    latest changes

--- assignments/Corejava/core-java-assignment/Bankapp/src/Main.java
+++ assignments/Corejava/core-java-assignment/Bankapp/src/Main.java
@@ -1,67 +1,76 @@
-import Service.AccountService;
+import Service.*;
 import Model.Account;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        AccountService accountService = new AccountService();
+        //AccountService accountService = new AccountService();
+        //AccountServiceArrayList accountService = new AccountServiceArrayList();
+        //AccountServiceLinkedList accountService = new AccountServiceLinkedList();
+        //AccountServiceTreeMap accountService = new AccountServiceTreeMap();
+        //AccountServiceHashMap accountService = new AccountServiceHashMap();
+        AccountServiceHashSet accountService = new AccountServiceHashSet();
         Scanner scanner = new Scanner(System.in);
         int choice;
-        System.out.println("Account Management Menu:");
-        System.out.println("1. Create Account");
-        System.out.println("2. View All Accounts");
-        System.out.println("3. View Account by ID");
-        System.out.println("4. Update Account Balance");
-        System.out.println("5. Delete Account");
-        System.out.println("6. Exit");
-        System.out.print("Enter your choice: ");
-        choice = scanner.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.print("Enter Account ID: ");
-                int id = scanner.nextInt();
-                System.out.print("Enter Account Name: ");
-                String name = scanner.next();
-                System.out.print("Enter Account Balance: ");
-                double balance = scanner.nextDouble();
-                System.out.print("Enter Account Type: ");
-                String type = scanner.next();
-                accountService.createAccount(id, name, balance, type);
-                break;
-            case 2:
-                Account[] allAccounts = accountService.getAllAccounts();
-                for (Account account : allAccounts) {
-                    System.out.println(account);
-                    System.out.println();
-                }
-                break;
-            case 3:
-                System.out.print("Enter Account ID to view: ");
-                int viewId = scanner.nextInt();
-                Account viewAccount = accountService.getAccountById(viewId);
-                if (viewAccount != null) {
-                    System.out.println(viewAccount);
-                } else {
-                    System.out.println("Account not found.");
-                }
-                break;
-            case 4:
-                System.out.print("Enter Account ID to update balance: ");
-                int updateId = scanner.nextInt();
-                System.out.print("Enter New Balance: ");
-                double newBalance = scanner.nextDouble();
-                accountService.updateBalance(updateId, newBalance);
-                break;
-            case 5:
-                System.out.print("Enter Account ID to delete: ");
-                int deleteId = scanner.nextInt();
-                accountService.deleteAccount(deleteId);
-                break;
-            case 6:
-                System.out.println("Exiting the application.");
-                break;
-            default:
-                System.out.println("Invalid choice. Please try again.");
-        }
+        do {
+            System.out.println("BANK ACCOUNT APPLICATION");
+            System.out.println("1) Create Account");
+            System.out.println("2) Read Account");
+            System.out.println("3) Read Account by ID");
+            System.out.println("4) Update Account");
+            System.out.println("5) Delete Account");
+            System.out.println("6) Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter Account ID: ");
+                    int id = scanner.nextInt();
+                    System.out.print("Enter Account Name: ");
+                    String name = scanner.next();
+                    System.out.print("Enter Account Balance: ");
+                    double balance = scanner.nextDouble();
+                    System.out.print("Enter Account Type: ");
+                    String type = scanner.next();
+                    accountService.createAccount(id, name, balance, type);
+                    break;
+                case 2:
+                    Account[] allAccounts = accountService.getAllAccounts();
+                    for (Account account : allAccounts) {
+                        System.out.println(account);
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.print("Enter Account ID to Read: ");
+                    int viewId = scanner.nextInt();
+                    Account viewAccount = accountService.getAccountById(viewId);
+                    if (viewAccount != null) {
+                        System.out.println(viewAccount);
+                    } else {
+                        System.out.println("Account not found.");
+                    }
+                    break;
+                case 4:
+                    System.out.print("Enter Account ID to update: ");
+                    int updateId = scanner.nextInt();
+                    System.out.print("Enter New Balance: ");
+                    double newBalance = scanner.nextDouble();
+                    accountService.updateBalance(updateId, newBalance);
+                    break;
+                case 5:
+                    System.out.print("Enter Account ID to delete: ");
+                    int deleteId = scanner.nextInt();
+                    accountService.deleteAccount(deleteId);
+                    break;
+                case 6:
+                    System.out.println("Exiting the application.");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 6);
+        scanner.close();
     }
 }
 
---
+++ assignments/Corejava/core-java-assignment/Bankapp/src/Service/AccountServiceTreeMap.java
@@ -0,0 +1,41 @@
+package Service;
+
+import Model.Account;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class AccountServiceTreeMap implements BankOperation {
+    private Map<Integer, Account> accountMap = new TreeMap<>();
+
+    @Override
+    public void createAccount(int id, String name, double balance, String type) {
+        Account newAccount = new Account(id, name, balance, type);
+        accountMap.put(id, newAccount);
+        System.out.println("Account Created Successfully");
+    }
+
+    @Override
+    public Account[] getAllAccounts() {
+        return accountMap.values().toArray(new Account[0]);
+    }
+
+    @Override
+    public Account getAccountById(int id) {
+        return accountMap.get(id);
+    }
+
+    @Override
+    public void updateBalance(int id, double newBalance) {
+        Account account = accountMap.get(id);
+        if (account != null) {
+            account.setBalance(newBalance);
+        }
+    }
+
+    @Override
+    public void deleteAccount(int id) {
+        accountMap.remove(id);
+        System.out.println("Account deleted successfully");
+    }
+}
